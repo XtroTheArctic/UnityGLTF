38f7b6ca273568b887f4620ff77b6a1eaac58dc8
Onur Er <onurer@gmail.com>
2026-08-14 14:06:51 -0700

    Deleted ScaleFactor in GLTFImporter by robertdorn83's request.

--- Editor/Scripts/GLTFImporter.cs
+++ Editor/Scripts/GLTFImporter.cs
@@ -1024,7 +1024,6 @@ namespace UnityGLTF
 			    BlendShapeFrameWeight = _blendShapeFrameWeight,
 			    CameraImport = _importCamera,
 			    DeduplicateResources = _deduplicateResources,
-                ScaleFactor = _scaleFactor,
 		    };
 
 		    using (var stream = File.OpenRead(projectFilePath))
